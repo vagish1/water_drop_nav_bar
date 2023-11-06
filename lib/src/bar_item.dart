3f775c1d711c6561646779f0d5d2963435cc8f4c
Vishal Pathak <yoyovishalpathak2004@gmail.com>
2023-11-06 15:20:28 +0530

    initial commit

---
+++ lib/src/bar_item.dart
@@ -0,0 +1,11 @@
+class BarItem {
+  /// Selected or active icon must be filled icon and complementary to inactive icon.
+  final String filledIcon;
+
+  /// Normal or inactive icon must be outlined icon and complementary to active icon.
+  final String outlinedIcon;
+  BarItem({
+    required this.filledIcon,
+    required this.outlinedIcon,
+  });
+}
